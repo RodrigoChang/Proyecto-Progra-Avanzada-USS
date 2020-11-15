9c37d589af9bb728b61d627ed9a59ef2f4504ac7
Fabian <fabianestefania@gmail.com>
2020-11-15 03:45:21 -0300

    Controlador identifica que tipo de usuario inicia sesion
    -identificacion de que usuario esta iniciando sesion
    -conexion con base de datos dependendo de que tipo de usuario se conecta
    -la ventana principal desplegada dependera de que tipo de usuario ingresa

--- Taller2/src/main/java/Modelo/Administrador.java
+++ Taller2/src/main/java/Modelo/Administrador.java
@@ -12,4 +12,54 @@ package Modelo;
 public class Administrador {
     int id;
     String login, clave, email;
+    private static Administrador instance;
+
+    public Administrador(int id, String login, String clave, String email) {
+        this.id = id;
+        this.login = login;
+        this.clave = clave;
+        this.email = email;
+    }
+
+    public Administrador() {
+    }
+    
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public static Administrador getInstance(){
+        if (instance == null)
+            instance = new Administrador();
+        return instance;
+    }
 }
